8e5aeb54f040b57d215b303fdf40f32aa4ec69c6
rdabrowski <test@test.com>
2021-07-10 22:06:37 +0200

    init

---
+++ src/main/java/com/hsbc/ratesapiscrapper/RatesapiScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.hsbc.ratesapiscrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatesapiScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatesapiScrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsbc/ratesapiscrapper/RatesapiScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsbc.ratesapiscrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatesapiScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
